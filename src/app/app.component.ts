e8fd8bccb44cf5222f195b1b5c448308e180fdd4
sergiodev91 <sergiodev91@gmail.com>
2024-04-27 09:48:23 -0400

    🔧 [ADD] Apply signal state

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,15 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { GeminiService } from './service/gemini.service';
 import { VERB_PROMPT } from './prompt/verb.prompt';
+
+interface Verb {
+  infinitive: string;
+  simple_past: string;
+  past_participle: string;
+  spanish_translation: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -13,14 +20,19 @@ import { VERB_PROMPT } from './prompt/verb.prompt';
 })
 export class AppComponent {
   public inputValue: string = '';
-  public response: any;
+  public response = signal<Verb | null>(null);
+  public isLoading = signal<boolean>(false);
+
 
   private _geminiService = inject(GeminiService);
 
   constructor() { }
 
   async processInput() {
+    this.isLoading.set(true);
     const result = await this._geminiService.generateText(VERB_PROMPT, this.inputValue);
-    this.response = JSON.parse(result);
+
+    this.response.set(JSON.parse(result));
+    this.isLoading.set(false);
   }
 }
